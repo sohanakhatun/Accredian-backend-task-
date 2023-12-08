b151246d4bfb6d3ad0fc8915cf7ff1583c4c5091
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2023-12-09 01:48:35 +0530

    Update port

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
+  port:"3360"
 });
 
 app.post("/register", async (req, res) => {
