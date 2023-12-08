130792cdb933756cf73565f998aaf0469439eb13
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2023-12-09 02:21:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
-  connectTimeout: 20000,
+  connectTimeout: 0,
 });
 
 app.post("/register", async (req, res) => {
